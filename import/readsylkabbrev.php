7ee27ae5fe1bdd1b83db4558cfcad12dc1a40f45
houjie <hh_163com@163.com>
2018-04-23 09:11:52 +0800

    optimize program structure

--- import/readsylkabbrev.php
+++ import/readsylkabbrev.php
@@ -15,9 +15,6 @@ $table_name="abbreviations";
 $exportSYLK_file="../exportSYLK/Abbreviations";
 $insertCountAtOnce=500;
 $executeSQL=true;
-if($executeSQL){
-	$db = getDBClass();
-}
 /*
 C;Y1;X1;K"1103"
 C;X2;K"(12-4"
@@ -34,73 +31,6 @@ $last_colkey="X3";
 </head>
 
 <body>
-<?php
-echo"trying to write to kamila database table {$table_name}<br/>";
-$previous_ref="";
-$handle = fopen("$exportSYLK_file", "r");
-//if(!isset($handle)) echo "can not open file";
-if(!$handle) {echo "can not open file"; exit;}
-echo "Start time:".date("Y-m-d-H.i.s", time())."<br>";
-if($executeSQL){
-	echo "clear {$table_name}<br>";
-	commonClear($table_name,$db);
-}
-echo "scan {$table_name} data<br>";
-$outputfile = fopen($outputPath . "kamila_".$table_name . date("Y-m-d-H.i.s", time()) . ".sql", "w") or die("Unable to open file!");
-//clear table
-$fileLine=commonDeleteSQL($table_name,null);	
-fwrite($outputfile, $fileLine. ";\n");
-$recordCount=0;
-$fileLine="";
-while (($buffer = fgetCRstr($handle)) !== false) {    	
-	list($pos1,$pos2,$pos3,$pos4) = explode(";", $buffer);
-	if( $pos1 == "C" )
-	{ 
-		$col_key="";
-		$col_value="";
-		if(substr($pos2,0,1)=="Y"){
-			$recno = substr($pos2,1);
-			$record_data=array();
-			$record_data[$data_reference["X0"]]=$recno;	
-			$col_key=$pos3;
-			$col_value=inquotes($pos4);		
-		}else{
-			$col_key=$pos2;
-			$col_value=inquotes($pos3);
-		}
-		$col_name=$data_reference[$col_key];
-		if(!empty($col_name)){
-			$record_data[$col_name]=$col_value;
-		}
-		if($col_key==$last_colkey){
-			$record_data=myaddslashes($record_data);
-			if($executeSQL){
-				$result=commonInsert($table_name,$db, $record_data);
-			}
-//			echo $result["msg"]."<br>";
-			$insertPair=getInsertPair($record_data);
-			$recordCount++;
-			if($recordCount%$insertCountAtOnce==1){
-				$fileLine="insert into {$table_name} ({$insertPair[0]}) values \n";	
-				$fileLine.="($insertPair[1])";			
-			}else if($recordCount%$insertCountAtOnce==0){
-				$fileLine.=",\n($insertPair[1]);\n";
-				fwrite($outputfile, $fileLine);	
-			}else{
-				$fileLine.=",\n($insertPair[1])";
-			}						
-		}
-	}	// of if pos1==C
-}	//of while
-$fileLine.=";\n";
-fwrite($outputfile, $fileLine);	
-echo $recno;
-fclose($handle);
-fclose($outputfile);
-if($executeSQL){
-	$db->close();
-}
-echo "<br>"."End time:".date("Y-m-d-H.i.s", time());
-?>
+<?php require_once "commonReadSYLK.php" ?>
 </body>
 </html>
